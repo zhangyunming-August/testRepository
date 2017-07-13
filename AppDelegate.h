bdfce0a6705a6e9b8132e8cbcd34f0a261f2098f
zym <zym@mobimedical.cn>
2017-07-13 14:11:49 +0800

    添加文件

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataTest
+//
+//  Created by zym on 2017/7/5.
+//  Copyright © 2017年 Chendu E-hand Medical Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
